54b93e517b717ed7aaa639434d99181234bcb02c
Md.  Shahadat Hossain <51829255+ShahadatPOM@users.noreply.github.com>
2019-07-13 19:44:52 +0600

    Add files via upload

---
+++ asidebar.php
@@ -0,0 +1,44 @@
+        
+<div class="grid_2">
+    <div class="box sidemenu">
+        <div class="block" id="section-menu">
+            <ul class="section menu">
+               <li><a class="menuitem">Site Option</a>
+                    <ul class="submenu">
+                        <li><a href="titleslogan.php">Title & Slogan</a></li>
+                        <li><a href="social.php">Social Media</a></li>
+                        <li><a href="copyright.php">Copyright</a></li>
+                        
+                    </ul>
+                </li>
+				
+                 <li><a class="menuitem">Pages</a>
+                    <ul class="submenu">
+                        <li><a href="addpage.php">Add New Page</a> </li>
+                        <?php 
+                        $sql = "SELECT * FROM tbl_page";
+                        $get = $db->select($sql);
+                        if($get){
+                            while($result = $get->fetch_assoc()){
+
+                         ?>
+                        <li><a href="page.php?pageid=<?php echo $result['id']; ?>"><?php echo $result['pagename']; ?></a></li>
+                    <?php } } ?>
+                    </ul>
+                </li>
+                <li><a class="menuitem">Category Option</a>
+                    <ul class="submenu">
+                        <li><a href="addcat.php">Add Category</a> </li>
+                        <li><a href="catlist.php">Category List</a> </li>
+                    </ul>
+                </li>
+                <li><a class="menuitem">Post Option</a>
+                    <ul class="submenu">
+                        <li><a href="addpost.php">Add Post</a> </li>
+                        <li><a href="postlist.php">Post List</a> </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
